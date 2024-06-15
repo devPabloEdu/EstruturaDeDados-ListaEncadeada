f58f703b4f00de8a1c1e6f2e29f206697a3366f8
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-06-15 14:39:17 -0300

    finalizando o projeto e adicionando um readme

---
+++ README.md
@@ -0,0 +1,8 @@
+Olá, pessoal! 🌟
+
+Estou animado em compartilhar uma nova implementação em que estive trabalhando: uma Lista Encadeada (LinkedList) em Java. Esta é uma estrutura de dados fundamental, super útil em diversos contextos, e estou feliz em dividir o que aprendi com vocês!
+
+🛠️ Recursos da Implementação:
+Adição e Remoção de Elementos: Adicione ou remova elementos no início e no final da lista de forma eficiente.
+Acesso por Índice: Acesse e insira elementos em qualquer posição da lista.
+Visualização de Conteúdo: Exiba o conteúdo, o comprimento da lista, e os elementos cabeça e cauda.
--- src/main/java/dataStructure/LinkedList.java
+++ src/main/java/dataStructure/LinkedList.java
@@ -92,21 +92,63 @@ public class LinkedList {
     public void prepend(String data) {
         Node newNode = new Node(data);
         if (length ==0) {
-            head == new Node;
+            head = newNode;
             tail = newNode;
         } else {
             newNode.next = head;
             head = newNode;
         }
         length++;
-        
+
+    }
+
+    public Node removeFirst(){
+        if (length == 0) return null;
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+        return temp;
+    }
+
+    public Node get(int index) {
+        if (index < 0 || index >= length) return null;
+        Node temp = head;
+        for(int i = 0; i<index; i++){
+            temp = temp.next;
+        }
+        return  temp;
+    }
+
+    public boolean insert(int index, String data) {
+        if (index < 0 || index >= length) return false;
+        if (index == 0) {
+            prepend(data);
+            return true;
+        }
+        if (index == length) {
+            append(data);
+            return true;
+        }
+        Node newNode = new Node(data);
+        Node temp = get(index - 1);
+        newNode.next = temp.next;
+        temp.next = newNode;
+        length++;
+        return true;
     }
 
     public static void main(String[] args) {
         LinkedList list = new LinkedList("elemento 1");
         list.append("elemento 2");
         list.append("elemento 3");
-        System.out.println(list.removeLast().data);
+        list.insert(2, "elemento 2.5");
+
+
 
         list.getHead();
         list.getTail();
